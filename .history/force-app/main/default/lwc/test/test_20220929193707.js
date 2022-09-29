695023ef3b78bdcc4984a78f13c00e6730c7dba9
Devbrat Chauhan <chauhandevbrat309@gmail.com>
2022-09-29 19:41:03 +0530

    changes for test lwc is completed

--- .history/force-app/main/default/lwc/test/test_20220929193707.js
+++ .history/force-app/main/default/lwc/test/test_20220929193707.js
@@ -3,5 +3,4 @@ import { LightningElement, api, wire, track } from 'lwc';
 export default class Test extends LightningElement {
     @api githubPull;
     @track lwcProject;
-    message = "this is the my first repo connected with github";
 }
